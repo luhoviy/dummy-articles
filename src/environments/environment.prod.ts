de03c6494be77e5db17227d4b9a9c4e7d942124e
Ruslan Luhoviy <ruslan.luhoviy@sombrainc.com>
2021-09-11 16:26:39 +0300

    firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA_85lkbLypataHRF-o34aW5n2SjPwMZ-A",
+    authDomain: "dummy-articles.firebaseapp.com",
+    projectId: "dummy-articles",
+    storageBucket: "dummy-articles.appspot.com",
+    messagingSenderId: "974748725538",
+    appId: "1:974748725538:web:254b4acedf32958a4cb57b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA_85lkbLypataHRF-o34aW5n2SjPwMZ-A",
+    authDomain: "dummy-articles.firebaseapp.com",
+    projectId: "dummy-articles",
+    storageBucket: "dummy-articles.appspot.com",
+    messagingSenderId: "974748725538",
+    appId: "1:974748725538:web:254b4acedf32958a4cb57b"
+  }
 };
 
 /*
